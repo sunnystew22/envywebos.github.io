27f4dcaabdc0cd4a36a4b7e81c3a28122ac6fe85
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2023-09-13 19:25:14 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -55,7 +55,7 @@ if (/Mobi/.test(navigator.userAgent) === true) location.replace('mobile/index.ht
         right: 0,
         bottom: 35,
         left: 0,
-        url: "https://invidious.private.coffee/feed/popular",
+        url: "https://invidious.slipfox.xyz/webhp?igu=1",
       });
     }
 
